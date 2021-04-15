a57a3c2c2332aa39035feaf0bbff4bc0368ed6c5
rugile <rugile.geceviciute@gmail.com>
2021-04-15 21:24:46 +0300

    seeder

--- lara_parduotuve/database/seeders/DatabaseSeeder.php
+++ lara_parduotuve/database/seeders/DatabaseSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace Database\Seeders;
-
+use Hash;
+ use DB;
+ use Faker\Factory as Faker;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,29 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Rugile',
+            'email' => 'rugile@gmail.com',
+            'password' => Hash::make('123'),
+        ]);
+        $faker = Faker::create('lt_LT');
+        $fakerCar = (new \Faker\Factory())::create();
+        $fakerCar->addProvider(new \Faker\Provider\Fakecar($fakerCar));
+        $makers = 5;
+        $cars = 5;
+        foreach(range(1, $makers) as $_) {
+            DB::table('makers')->insert([
+                'name' => $faker->firstName(),
+                
+            ]);
+        }
+        foreach(range(1, $cars) as $_) {
+            DB::table('cars')->insert([
+                'name' => $fakerCar->vehicleType(),
+                'plate' => $fakerCar->vehicleRegistration('[A-Z]{2}-[0-9]{5}'),
+                'about' => $faker->realText(400, 4),
+                'maker_id' => rand(1, $makers),
+            ]);
+        }
     }
 }
